a6ab6750a91301ac45c028b51eddb68d95eeeac1
Abhijan <127745795+Abhijanbasyal@users.noreply.github.com>
2024-03-15 13:55:01 +0545

    Delete back-end directory

--- back-end/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    JWT_SECRET : "ANushISGay",
-    EMAIL : "maureen.steuber@ethereal.email",
-    PASSWORD: "	PRyexuwGWACWpPDctm",
-    ATLAS_URI: "mongodb+srv://np01cp4s220138:funnyguy@cluster0.n5i1thl.mongodb.net/?retryWrites=true&w=majority"
-}
